42c658560f3a99088de1968f4917b3d7e28e7120
efe <e.sozen@student.tudelft.nl>
2024-11-18 14:09:33 +0100

    benefits page

--- components/layout/sections/benefits.tsx
+++ components/layout/sections/benefits.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AnimatedBeamMultipleOutputDemo } from "@/components/ui/feed-sources";
 import { Icon } from "@/components/ui/icon";
 import { icons } from "lucide-react";
 
@@ -11,27 +12,27 @@ interface BenefitsProps {
 const benefitList: BenefitsProps[] = [
   {
     icon: "Blocks",
-    title: "Build Brand Trust",
+    title: "Increase Visibility",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio velit cum aliquam. Natus consectetur dolores.",
+      "Boost your rankings on AI-driven search platforms to ensure your brand is seen by a broader and more relevant audience.",
   },
   {
     icon: "LineChart",
-    title: "More Leads",
+    title: "Drive Targeted Traffic",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio velit cum aliquam, natus consectetur.",
-  },
-  {
-    icon: "Wallet",
-    title: "Higher Conversions",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus consectetur. A odio velit cum aliquam",
+      "Optimize for AI searches to attract highly targeted users who are actively looking for solutions you provide.",
   },
   {
     icon: "Sparkle",
-    title: "Test Marketing Ideas",
+    title: "Stay Ahead of Trends",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio velit cum aliquam. Natus consectetur dolores.",
+      "Leverage the latest in AI search technology to maintain a competitive edge in your industry and adapt to changing user behavior.",
+  },
+  {
+    icon: "Lightbulb",
+    title: "Optimize Content Strategy",
+    description:
+      "Gain insights into AI search trends to create content that resonates with your audience and aligns with search algorithms.",
   },
 ];
 
@@ -39,8 +40,7 @@ export const BenefitsSection = () => {
   return (
     <section id="benefits" className="container py-24 sm:py-32">
       <div className="grid lg:grid-cols-2 place-items-center lg:gap-24">
-        <div>
-          <h2 className="text-lg text-primary mb-2 tracking-wider">Benefits</h2>
+        {/* <h2 className="text-lg text-primary mb-2 tracking-wider">Benefits</h2>
 
           <h2 className="text-3xl md:text-4xl font-bold mb-4">
             Your Shortcut to Success
@@ -49,9 +49,8 @@ export const BenefitsSection = () => {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
             ducimus reprehenderit architecto rerum similique facere odit
             deleniti necessitatibus quo quae.
-          </p>
-        </div>
-
+          </p> */}
+        <AnimatedBeamMultipleOutputDemo />
         <div className="grid lg:grid-cols-2 gap-4 w-full">
           {benefitList.map(({ icon, title, description }, index) => (
             <Card
@@ -74,9 +73,7 @@ export const BenefitsSection = () => {
                 <CardTitle>{title}</CardTitle>
               </CardHeader>
 
-              <CardContent className="text-muted-foreground">
-                {description}
-              </CardContent>
+              <CardContent className="text-muted-foreground">{description}</CardContent>
             </Card>
           ))}
         </div>
