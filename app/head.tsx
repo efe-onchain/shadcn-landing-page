a0e3133b8134d93d1f026d843d75e973a2111905
efe <e.sozen@student.tudelft.nl>
2024-11-23 20:06:19 +0100

    seline.so

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.json" />
       <meta name="OAI-SearchBot" content="index,follow" />
+      <script async src="https://cdn.seline.so/seline.js" data-token="e38ab5637034c9e"></script>
       <meta name="PerplexityBot" content="index,follow" />
     </>
   );
